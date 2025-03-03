f0669ea1eede11b7c21554bd5e74abc88eea5b2e
Nur-Ananda4 <mickhaelvins35@gmail.com>
2025-03-03 19:39:54 +0700

    Add files via upload

---
+++ Tugas 1/kode program/belajar java.java
@@ -0,0 +1,22 @@
+public class Deklarasi{
+//var instance
+int kelas, ukuran ;
+private string nama="Nanda";
+protected char laki='L', perempuan='P';
+
+//class variabel
+static string nilai;
+
+//konstan variabel
+final double PHI = 3.14
+
+public Deklarasi(){
+
+kelas = 4;
+ukuran = 10;
+nama = "Aisyah";
+string variabellokal ="ini variabel lokal";
+
+}
+}
+
